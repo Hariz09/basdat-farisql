5eb46856f48d69f93c7a3d69b5bfe6c5aac0def7
Gerrybp12 <gerry.bimaputra@gmail.com>
2026-04-30 12:41:31 +0700

    changes: guest cant access artist

--- components/layout/GuestNavbar.tsx
+++ components/layout/GuestNavbar.tsx
@@ -4,7 +4,6 @@ export function GuestNavbar() {
   return (
     <AppHeader
       items={[
-        { href: "/artists", label: "Artis" },
         { href: "/ticket-categories", label: "Kategori Tiket" },
         { href: "/login", label: "Login" },
         { href: "/register", label: "Registrasi" },
